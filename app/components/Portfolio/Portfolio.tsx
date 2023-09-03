a61c75dd78b883c6b34c4ca00ae3175a6aba7fbd
Facundo David Gonzalez <fac.gonzalez@outlook.com>
2023-09-03 13:07:08 -0300

    add project cards

--- app/components/Portfolio/Portfolio.tsx
+++ app/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,7 @@
 import { PContainer, PTitles } from "app/components";
 import styles from "./Portfolio.module.css";
+import { ProjectCard } from "./components";
+import Image from "next/image";
 
 function Portfolio() {
   return (
@@ -7,7 +9,37 @@ function Portfolio() {
       <PContainer>
         <div className={styles.content}>
           <PTitles section="PORTFOLIO" title="Here are some project I done." />
-          <div style={{ height: 200 }}></div>
+          <ProjectCard
+            liveDemo="https://movie-catalog-gamma.vercel.app/"
+            title="Movie Catalog"
+            desc="A movie catalog website, a place to see and search some information like rating, duration, genres, etc. about popular, now playing or upcoming movies. The information is retrive thanks to themoviedb api."
+            stack={["React", "TMDB API"]}
+            code="https://github.com/facu1/movie-catalog"
+          >
+            <Image
+              src="/imgs/movie-catalog.png"
+              alt="movie catalog preview"
+              width={1000}
+              height={1000}
+              className={`${styles.img} ${styles.img1}`}
+            />
+          </ProjectCard>
+          <ProjectCard
+            reverse
+            liveDemo="https://wedding-invitation-demo.vercel.app/"
+            title="Online Invitations"
+            desc="Online invitations, a personal project to make invitations for parties. The invitations are customizable to fit the event. Currently, 3 invitations have been delivered successfully and a 4th is on the way."
+            stack={["React"]}
+            code="https://github.com/facu1/xv-invitation"
+          >
+            <Image
+              src="/imgs/wedding-invitation.png"
+              alt="wedding invitation preview"
+              width={1000}
+              height={1000}
+              className={`${styles.img} ${styles.img2}`}
+            />
+          </ProjectCard>
         </div>
       </PContainer>
     </div>
---
+++ app/components/Portfolio/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,74 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./ProjectCard.module.css";
+import { ReactElement } from "react";
+
+interface Props {
+  reverse?: boolean;
+  liveDemo: string;
+  title: string;
+  desc: string;
+  stack: string[];
+  code: string;
+  children: ReactElement;
+}
+
+function ProjectCard({
+  reverse = false,
+  liveDemo,
+  title,
+  desc,
+  stack,
+  code,
+  children,
+}: Props) {
+  return (
+    <div className={`${styles.card} ${reverse ? styles.reverse : ""}`}>
+      <div className={styles["img-container"]}>
+        <a
+          href={liveDemo}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          {children}
+        </a>
+      </div>
+      <div className={styles["txt-container"]}>
+        <h1>{title}</h1>
+        <p>{desc}</p>
+        <div className={styles.stack}>
+          {stack.map((tech) => (
+            <h2 key={tech}>{tech}</h2>
+          ))}
+        </div>
+        <div className={styles.links}>
+          <a
+            href={code}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            <p>Code</p>
+            <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+          </a>
+          <a
+            href={liveDemo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            <p>Live Demo</p>
+            <FontAwesomeIcon
+              icon={faArrowUpRightFromSquare}
+              className={styles.icon}
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
